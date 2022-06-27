75582674a74a1903152968d0a93352911541998f
JacasMiran <rodolforafael15@gmail.com>
2022-06-26 23:17:40 -0300

    DogClass finished

--- Chapter1/TheDogClass.py
+++ Chapter1/TheDogClass.py
@@ -1,15 +1,15 @@
 class Dog:
-# This is the constructor for the class. It is called whenever
-# object is created. The reference called "self" is created by
-# and made to point to the space for the newly created object.
-# does this automatically for us but we have to have "self" as
-# parameter to the __init__ method (i.e. the constructor).
+    # This is the constructor for the class. It is called whenever
+    # object is created. The reference called "self" is created by
+    # and made to point to the space for the newly created object.
+    # does this automatically for us but we have to have "self" as
+    # parameter to the __init__ method (i.e. the constructor).
     def __init__(self, name, month, day, year, speakText):
-    self.name = name
-    self.month = month
-    self.day = day
-    self.year = year
-    self.speakText = speakText
+        self.name = name
+        self.month = month
+        self.day = day
+        self.year = year
+        self.speakText = speakText
     # This is an accessor method that returns the speakText stored in the
     # object. Notice that "self" is a parameter. Every method has "self" as its
     # first parameter. The "self" parameter is a reference to the current
@@ -19,4 +19,34 @@ class Dog:
         return self.speakText
     def getName(self):
         return self.name
+
+    # This is another accessor method that uses the birthday information to
+    # return a string representing the date.
+    def birthDate(self):
+        return str(self.month) + "/" + str(self.day) + "/" + str(self.year)
+
+    # This is a mutator method that changes the speakText of the Dog object.
+    def changeBark(self,bark):
+        self.speakText = bark
+    def __add__(self,otherDog):
+        return Dog("Puppy of " + self.name + " and " + otherDog.name, \
+        self.month, self.day, self.year + 1, \
+        self.speakText + otherDog.speakText)
+
+def main():
+    boyDog = Dog("Mesa", 5, 15, 2004, "WOOOOF")
+    girlDog = Dog("Sequoia", 5, 6, 2004, "barkbark")
+    print(boyDog.speak())
+    print(girlDog.speak())
+    print(boyDog.birthDate())
+    print(girlDog.birthDate())
+    boyDog.changeBark("woofywoofy")
+    print(boyDog.speak())
+    puppy = boyDog + girlDog
+    print(puppy.speak())
+    print(puppy.getName())
+    print(puppy.birthDate())
+
+if __name__ == "__main__":
+    main()
     
